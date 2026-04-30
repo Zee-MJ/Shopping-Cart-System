c36e8ea69a9d7572cf48ad9966f489defa682ffd
Zee-MJ <santoszyrah175@gmail.com>
2026-05-01 06:39:34 +0800

    Fixing receipt code

--- ShoppingCart/obj/Debug/net10.0/ShoppingCart.AssemblyInfo.cs
+++ ShoppingCart/obj/Debug/net10.0/ShoppingCart.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e82f722480b445676582606a462c9e950954c133")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bde385c401517e5d94c721fdb8a38baf852dde3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
